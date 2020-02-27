e4b95775629e9b090ce8df62b34ca70ac8874b5f
marina-igorevna-mishur <marina.igorevna.mishur@gmail.com>
2020-02-27 16:45:07 +0300

    react

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const renderApp = () => {
+    ReactDOM.render(
+        React.createElement(App),
+        document.getElementById('root')
+    )
+}
+
+renderApp()
